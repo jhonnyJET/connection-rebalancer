3936d440c61465cabfe49f37afcc5917ea13d957
Jonathas Santos <jonathas.santos@justeattakeaway.com>
2026-02-10 17:23:38 -0700

    WC-XX [BE] add properties to bypass cors and update package script

--- package.sh
+++ package.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-mvn package -Dquarkus.container-image.build=true -Dquarkus.package.type=jar -Dquarkus.container-image.tag=jvm.test
+#!/usr/bin/env bash
+
+# 0. capturing arguments...
+while getopts "v:a:" flag
+do
+    case "${flag}" in
+        v) version=$OPTARG;;
+        a) arch=$OPTARG;;
+    esac
+done
+
+echo "Version: $version";
+echo "Arch: $arch";
+
+mvn package -Dquarkus.container-image.build=true -Dquarkus.package.type=jar -Dquarkus.container-image.tag=${version}-${arch}
