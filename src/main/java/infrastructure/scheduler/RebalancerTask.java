7ef9414dc85c41f74502a049eff002bcc06bffca
Jonathas Santos <jonathas.santos@justeattakeaway.com>
2026-02-23 18:18:01 -0700

    WC-XX [BE] raise rebalancing schedule to 60s

--- src/main/java/infrastructure/scheduler/RebalancerTask.java
+++ src/main/java/infrastructure/scheduler/RebalancerTask.java
@@ -4,7 +4,6 @@ import api.WsSessionApi;
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,7 +13,7 @@ public class RebalancerTask {
     @Inject
     private WsSessionApi wsSessionApi;
 
-    @Scheduled(every = "30s")
+    @Scheduled(every = "60s")
     public void analyzeConnectionRebalance(){
         // Logger.getAnonymousLogger().log(Level.INFO, "Starting session rebalance");
         wsSessionApi.analyzeSessionServerBalance();
